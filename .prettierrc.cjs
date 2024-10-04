77cd48520fc65f97aa7c056e34f8c563f93c5908
Artyom D <d.tem4ik@gmail.com>
2024-10-04 11:08:56 +0500

    add/prettier-congif

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  vueIndentScriptAndStyle: true,
+  useTabs: false,
+  bracketSpcing: true,
+  endOfLine: 'auto',
+  formatOnSave: true,
+}
