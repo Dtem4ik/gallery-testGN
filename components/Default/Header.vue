817f438e7e990d7a449365611ca138bad36464ad
Artyom D <d.tem4ik@gmail.com>
2024-10-04 11:09:55 +0500

    add/header

---
+++ components/Default/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="header px-24 pt-24 pb-16">
+    <div class="header__body card rounded-16 bg-white py-16 px-24">
+      <h1 class="header__title font-size-18 line-height-24">
+        Тестовое задание
+      </h1>
+    </div>
+  </header>
+</template>
